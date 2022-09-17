b641ba162037811c2457fed3147df09864f226c5
patri-create <patriciamkv@gmail.com>
2022-09-17 14:11:42 +0200

    feat: add splash screen

--- app/src/main/java/com/project/mymarvel/MainActivity.kt
+++ app/src/main/java/com/project/mymarvel/MainActivity.kt
@@ -2,10 +2,17 @@ package com.project.mymarvel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showSplash()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun showSplash() {
+        Thread.sleep(800)
+        installSplashScreen()
     }
 }
