f4fb853f2a2415ba8d9008c902775ccfac1bf254
patri-create <patriciamkv@gmail.com>
2022-09-18 16:19:05 +0200

    feat: add design for recyclerView

--- app/src/main/java/com/project/mymarvel/domain/Heroes.kt
+++
@@ -1,4 +0,0 @@
-package com.project.mymarvel.domain
-
-class Heroes(override var image: String, override var title: String) : MarvelItem {
-}
--- app/src/main/java/com/project/mymarvel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/project/mymarvel/ui/home/HomeFragment.kt
@@ -7,8 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.project.mymarvel.R
 import com.project.mymarvel.databinding.FragmentHomeBinding
-import com.project.mymarvel.domain.Heroes
-import com.project.mymarvel.domain.MarvelItem
+import com.project.mymarvel.domain.Hero
 
 class HomeFragment : Fragment() {
 
@@ -34,13 +33,14 @@ class HomeFragment : Fragment() {
 
     private fun instances() {
         val recycler = binding.recycler
+        recycler.addItemDecoration(MarginItemDecoration(resources.getDimensionPixelSize(R.dimen.margin)))
         recycler.adapter = HomeAdapter(
             listOf(
-                Heroes(
+                Hero(
                     "https://media.vandalsports.com/i/640x360/4-2021/2021427125442_1.jpg",
                     "Capitán"
                 ),
-                Heroes(
+                Hero(
                     "https://static.wikia.nocookie.net/marveldatabase/images/c/c8/Wanda_Maximoff_%28Earth-199999%29_from_Doctor_Strange_in_the_Multiverse_of_Madness_Promo_001.jpg/revision/latest?cb=20220504145159",
                     "Wanda"
                 )
