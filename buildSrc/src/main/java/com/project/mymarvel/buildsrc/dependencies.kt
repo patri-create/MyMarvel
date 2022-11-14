b355e9eff195b726019ed2aa3a63525e6957f109
patri-create <patriciamkv@gmail.com>
2022-11-14 22:43:31 +0100

    feat: add dependency updates task

--- buildSrc/src/main/java/com/project/mymarvel/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/project/mymarvel/buildsrc/dependencies.kt
@@ -3,6 +3,7 @@ package com.project.mymarvel.buildsrc
 object Libs {
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.2.2"
+    const val gradleVersionsPlugin = "com.github.ben-manes:gradle-versions-plugin:0.42.0"
 
     object Kotlin {
         private const val version = "1.7.10"
