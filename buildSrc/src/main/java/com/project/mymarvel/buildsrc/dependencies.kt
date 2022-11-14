fecfaa580d9a09951b59f3cb6b2f07df2537eda0
patri-create <patriciamkv@gmail.com>
2022-11-14 22:25:34 +0100

    feat: add buildSrc

---
+++ buildSrc/src/main/java/com/project/mymarvel/buildsrc/dependencies.kt
@@ -0,0 +1,11 @@
+package com.project.mymarvel.buildsrc
+
+object Libs {
+
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.2.2"
+
+    object Kotlin {
+        private const val version = "1.7.10"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+    }
+}
