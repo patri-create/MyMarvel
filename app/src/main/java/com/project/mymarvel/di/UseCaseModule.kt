a7995ccabee6331315574d933d9b42b85732fc04
patri-create <patriciamkv@gmail.com>
2022-12-03 20:00:10 +0100

    refactor: remove unused code

--- app/src/main/java/com/project/mymarvel/di/UseCaseModule.kt
+++ app/src/main/java/com/project/mymarvel/di/UseCaseModule.kt
@@ -6,18 +6,21 @@ import com.project.mymarvel.usecases.FindHeroesUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object UseCaseModule {
 
     @Provides
+    @ViewModelScoped
     fun providesFindHeroesUseCase(marvelRepository: MarvelRepository): FindHeroesUseCase {
         return FindHeroesUseCase(marvelRepository)
     }
 
     @Provides
+    @ViewModelScoped
     fun providesFindEventsUseCase(marvelRepository: MarvelRepository): FindEventsUseCase {
         return FindEventsUseCase(marvelRepository)
     }
