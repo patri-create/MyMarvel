def0ea4939cfb76f27aa456b4f3cdabd7ef330f2
patri-create <patriciamkv@gmail.com>
2022-12-05 13:09:06 +0100

    feat: add HomeViewModel test

---
+++ testSamples/src/main/java/com/project/mymarvel/testsamples/samples.kt
@@ -0,0 +1,17 @@
+package com.project.mymarvel.testsamples
+
+import com.project.mymarvel.domain.Hero
+import com.project.mymarvel.domain.Event
+
+val sampleHero = Hero(
+    0,
+    "Name",
+    "https://image"
+)
+
+val sampleEvent = Event(
+    0,
+    "https://image",
+    "Title",
+    "Description"
+)
